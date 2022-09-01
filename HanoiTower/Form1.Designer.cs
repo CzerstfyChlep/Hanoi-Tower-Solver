e500e4e029afbd9af7e6763491015ff4c306f3d1
CzerstfyChlep <35424664+CzerstfyChlep@users.noreply.github.com>
2022-09-01 17:41:35 +0200

    Add project files.

---
+++ HanoiTower/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace HanoiTower
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NextMoveButton = new System.Windows.Forms.Button();
+            this.ResetButton = new System.Windows.Forms.Button();
+            this.NumberOfPlates = new System.Windows.Forms.NumericUpDown();
+            this.NumberOfMoves = new System.Windows.Forms.Label();
+            this.AutomaticMove = new System.Windows.Forms.Button();
+            this.Exitbutton = new System.Windows.Forms.Button();
+            this.FastBox = new System.Windows.Forms.CheckBox();
+            this.SFastBox = new System.Windows.Forms.CheckBox();
+            this.ColoredBox = new System.Windows.Forms.CheckBox();
+            this.SaveToFile = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.NumberOfPlates)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // NextMoveButton
+            // 
+            this.NextMoveButton.Location = new System.Drawing.Point(356, 356);
+            this.NextMoveButton.Name = "NextMoveButton";
+            this.NextMoveButton.Size = new System.Drawing.Size(75, 23);
+            this.NextMoveButton.TabIndex = 0;
+            this.NextMoveButton.Text = "Next move";
+            this.NextMoveButton.UseVisualStyleBackColor = true;
+            this.NextMoveButton.Click += new System.EventHandler(this.NextMoveButton_Click);
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Location = new System.Drawing.Point(275, 356);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(75, 23);
+            this.ResetButton.TabIndex = 1;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // NumberOfPlates
+            // 
+            this.NumberOfPlates.Location = new System.Drawing.Point(206, 359);
+            this.NumberOfPlates.Name = "NumberOfPlates";
+            this.NumberOfPlates.Size = new System.Drawing.Size(63, 20);
+            this.NumberOfPlates.TabIndex = 2;
+            this.NumberOfPlates.ValueChanged += new System.EventHandler(this.NumberOfPlates_ValueChanged);
+            // 
+            // NumberOfMoves
+            // 
+            this.NumberOfMoves.AutoSize = true;
+            this.NumberOfMoves.Location = new System.Drawing.Point(437, 361);
+            this.NumberOfMoves.Name = "NumberOfMoves";
+            this.NumberOfMoves.Size = new System.Drawing.Size(46, 13);
+            this.NumberOfMoves.TabIndex = 3;
+            this.NumberOfMoves.Text = "Move: 0";
+            // 
+            // AutomaticMove
+            // 
+            this.AutomaticMove.Location = new System.Drawing.Point(771, 356);
+            this.AutomaticMove.Name = "AutomaticMove";
+            this.AutomaticMove.Size = new System.Drawing.Size(65, 23);
+            this.AutomaticMove.TabIndex = 4;
+            this.AutomaticMove.Text = "Auto";
+            this.AutomaticMove.UseVisualStyleBackColor = true;
+            this.AutomaticMove.Click += new System.EventHandler(this.AutomaticMove_Click);
+            // 
+            // Exitbutton
+            // 
+            this.Exitbutton.Location = new System.Drawing.Point(125, 356);
+            this.Exitbutton.Name = "Exitbutton";
+            this.Exitbutton.Size = new System.Drawing.Size(75, 23);
+            this.Exitbutton.TabIndex = 5;
+            this.Exitbutton.Text = "Exit";
+            this.Exitbutton.UseVisualStyleBackColor = true;
+            this.Exitbutton.Click += new System.EventHandler(this.Exitbutton_Click);
+            // 
+            // FastBox
+            // 
+            this.FastBox.AutoSize = true;
+            this.FastBox.Location = new System.Drawing.Point(592, 360);
+            this.FastBox.Name = "FastBox";
+            this.FastBox.Size = new System.Drawing.Size(46, 17);
+            this.FastBox.TabIndex = 6;
+            this.FastBox.Text = "Fast";
+            this.FastBox.UseVisualStyleBackColor = true;
+            // 
+            // SFastBox
+            // 
+            this.SFastBox.AutoSize = true;
+            this.SFastBox.Location = new System.Drawing.Point(644, 360);
+            this.SFastBox.Name = "SFastBox";
+            this.SFastBox.Size = new System.Drawing.Size(53, 17);
+            this.SFastBox.TabIndex = 7;
+            this.SFastBox.Text = "SFast";
+            this.SFastBox.UseVisualStyleBackColor = true;
+            // 
+            // ColoredBox
+            // 
+            this.ColoredBox.AutoSize = true;
+            this.ColoredBox.Location = new System.Drawing.Point(703, 360);
+            this.ColoredBox.Name = "ColoredBox";
+            this.ColoredBox.Size = new System.Drawing.Size(62, 17);
+            this.ColoredBox.TabIndex = 8;
+            this.ColoredBox.Text = "Colored";
+            this.ColoredBox.UseVisualStyleBackColor = true;
+            this.ColoredBox.CheckedChanged += new System.EventHandler(this.ColoredBox_CheckedChanged);
+            // 
+            // SaveToFile
+            // 
+            this.SaveToFile.Location = new System.Drawing.Point(842, 356);
+            this.SaveToFile.Name = "SaveToFile";
+            this.SaveToFile.Size = new System.Drawing.Size(111, 23);
+            this.SaveToFile.TabIndex = 9;
+            this.SaveToFile.Text = "Save to file";
+            this.SaveToFile.UseVisualStyleBackColor = true;
+            this.SaveToFile.Click += new System.EventHandler(this.SaveToFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1002, 391);
+            this.ControlBox = false;
+            this.Controls.Add(this.SaveToFile);
+            this.Controls.Add(this.ColoredBox);
+            this.Controls.Add(this.SFastBox);
+            this.Controls.Add(this.FastBox);
+            this.Controls.Add(this.Exitbutton);
+            this.Controls.Add(this.AutomaticMove);
+            this.Controls.Add(this.NumberOfMoves);
+            this.Controls.Add(this.NumberOfPlates);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.NextMoveButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Hanoi Tower Solver - by J.Woldon";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.NumberOfPlates)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button NextMoveButton;
+        private System.Windows.Forms.Button ResetButton;
+        private System.Windows.Forms.NumericUpDown NumberOfPlates;
+        private System.Windows.Forms.Label NumberOfMoves;
+        private System.Windows.Forms.Button AutomaticMove;
+        private System.Windows.Forms.Button Exitbutton;
+        private System.Windows.Forms.CheckBox FastBox;
+        private System.Windows.Forms.CheckBox SFastBox;
+        private System.Windows.Forms.CheckBox ColoredBox;
+        private System.Windows.Forms.Button SaveToFile;
+    }
+}
+
---
+++ HanoiTower/Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+
+namespace HanoiTower
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            CheckForIllegalCrossThreadCalls = false;
+            DoubleBuffered = true;
+            this.FormClosed += AppExit;
+            t = new Thread(AutoThread);
+            r = new Random();
+            SaveToFileThread = new Thread(SaveToFileMeth);
+            
+           
+
+        }
+        public int Move = 1;
+        public List<Plate> Plates = new List<Plate>();
+        Thread t;
+        Random r;
+        bool stopThread = false;
+        bool suspendThread = false;
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            e.Graphics.DrawRectangle(Pens.Black, new Rectangle(200, 30, 10, 300));
+            e.Graphics.DrawRectangle(Pens.Black, new Rectangle(500, 30, 10, 300));
+            e.Graphics.DrawRectangle(Pens.Black, new Rectangle(800, 30, 10, 300));
+
+            Plates = Plates.OrderByDescending(x => x.Size).ToList();
+            int[] numberofplatesinplaces = new int[3];
+            
+            foreach(Plate p in Plates)
+            {
+                if (p.Place == 3)
+                    p.Place = 0;
+                else if (p.Place == -1)
+                    p.Place = 2;
+                if(ColoredBox.Checked)
+                    e.Graphics.FillRectangle(new SolidBrush(p.c), new Rectangle(p.Place * 300+ 205 - 5*p.Size, 322 - numberofplatesinplaces[p.Place] * 10, 10 * p.Size, 10));
+                else
+                    e.Graphics.FillRectangle(Brushes.Black, new Rectangle(p.Place * 300 + 205 - 5 * p.Size, 322 - numberofplatesinplaces[p.Place] * 10, 10 * p.Size, 10));
+                numberofplatesinplaces[p.Place]++;
+            }
+            if (!SFastBox.Checked)
+            {
+                NumberOfMoves.Text = "Move: " + Move + "/" + (Math.Pow(2, (int)NumberOfPlates.Value));
+            }
+        }       
+        public void AppExit(object sender, EventArgs e)
+        {
+            stopThread = true;
+            
+        }
+        public void MakeMove()
+        {
+            if (Move < (Math.Pow(2, (int)NumberOfPlates.Value)))
+            {
+                foreach (Plate p in Plates)
+                {
+                    if (Move == Math.Pow(2, p.Size) * p.TimesMoved + Math.Pow(2, p.Size - 1))
+                    {
+                        if (p.Size % 2 == 0 + NumberOfPlates.Value % 2)
+                        {
+                            p.Place--;
+                            if (p.Place == -1)
+                                p.Place = 2;
+                            p.TimesMoved++;
+                        }
+                        else
+                        {
+                            p.Place++;
+                            if (p.Place == 3)
+                                p.Place = 0;
+                            p.TimesMoved++;
+                        }
+                    }
+                }
+                Move++;
+            }
+            this.Invalidate();
+        }
+        private void NextMoveButton_Click(object sender, EventArgs e)
+        {
+            MakeMove();
+        }
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            this.Invalidate();
+            Plates.Clear();
+            for(int a = 0; a < NumberOfPlates.Value; a++)
+            {
+                Plates.Add(new Plate(a + 1, Color.FromArgb(r.Next(30, 230), r.Next(30, 230), r.Next(30, 230))));
+            }
+            Move = 1;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AutomaticMove_Click(object sender, EventArgs e)
+        {
+            
+            if (t.ThreadState == ThreadState.Unstarted)
+            {
+                t.Start();
+                AutomaticMove.Text = "Stop";
+            }
+            else
+            {
+                if (!suspendThread)
+                {
+                    suspendThread = true;
+                    AutomaticMove.Text = "Resume";
+                }
+                else
+                {
+                    suspendThread = false;
+                    AutomaticMove.Text = "Stop";
+                }
+            }
+           
+
+        }
+        public void AutoThread()
+        {
+            while (true)
+            {
+                if (SFastBox.Checked)
+                {
+
+                }
+                   
+                else if(FastBox.Checked)
+                    Thread.Sleep(20);
+                else
+                    Thread.Sleep(100);
+                MakeMove();
+                if (stopThread)
+                    break;
+                sus:
+                if (suspendThread && !stopThread)
+                    goto sus;
+                else if (stopThread)
+                    break;
+                
+            }
+        }
+
+        private void Exitbutton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void NumberOfPlates_ValueChanged(object sender, EventArgs e)
+        {
+            this.Invalidate();
+            Plates.Clear();
+            for (int a = 0; a < NumberOfPlates.Value; a++)
+            {
+                Plates.Add(new Plate(a + 1, Color.FromArgb(r.Next(30, 230), r.Next(30, 230), r.Next(30, 230))));
+            }
+            Move = 1;
+        }
+
+        private void ColoredBox_CheckedChanged(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+        Thread SaveToFileThread; 
+        public void SaveToFileMeth()
+        {
+            List<Plate> LocalPlates = new List<Plate>();
+            File.WriteAllText("hanoi.txt", "");
+            int LocalMove = 1;
+            for (int a = 0; a < NumberOfPlates.Value; a++)
+            {
+                LocalPlates.Add(new Plate(a + 1, Color.FromArgb(r.Next(30, 230), r.Next(30, 230), r.Next(30, 230))));
+            }
+            
+            int maxmove = (int)(Math.Pow(2, (int)NumberOfPlates.Value));
+
+            do
+            {
+                if (stopThread)
+                {
+                    break;
+                }
+                string toAdd = "";
+                if (LocalMove < (Math.Pow(2, (int)NumberOfPlates.Value)))
+                {
+                    foreach (Plate p in LocalPlates)
+                    {
+
+                        if (LocalMove == Math.Pow(2, p.Size) * p.TimesMoved + Math.Pow(2, p.Size - 1))
+                        {
+                            toAdd += p.Place + "->";
+                            if (p.Size % 2 == 0 + NumberOfPlates.Value % 2)
+                            {
+                                p.Place--;
+                                if (p.Place == -1)
+                                    p.Place = 2;
+                                p.TimesMoved++;
+                            }
+                            else
+                            {
+                                p.Place++;
+                                if (p.Place == 3)
+                                    p.Place = 0;
+                                p.TimesMoved++;
+                            }
+                            toAdd += p.Place + "\n";
+                        }
+                    }
+                    LocalMove++;
+                }
+
+                File.AppendAllText("hanoi.txt", toAdd);
+            }
+            while (LocalMove < maxmove);
+            MessageBox.Show("Sequence succesfully saved to a text file");
+            
+
+        }
+
+        private void SaveToFile_Click(object sender, EventArgs e)
+        {
+            if(SaveToFileThread.ThreadState == ThreadState.Unstarted)
+                SaveToFileThread.Start();
+            else
+            {
+                SaveToFileThread = new Thread(SaveToFileMeth);
+                SaveToFileThread.Start();
+            }
+                              
+        }
+    }
+    public class Plate
+    {
+        public Plate(int size, Color C)
+        {
+            Size = size;
+            c = C;
+           
+        }
+        public int Size = 1;
+        public int TimesMoved = 0;
+        public int Place = 0;
+        public Color c = new Color();
+    }
+}
